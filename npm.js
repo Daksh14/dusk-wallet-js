36e1bbd89064d327615d32f7361c5908af58fa34
Daksh <work@dakshu.xyz>
2023-11-15 10:08:05 -0500

    Add npm.js build script

---
+++ npm.js
@@ -0,0 +1,31 @@
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/mod.js"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "dusk-wallet-js",
+    version: Deno.args[0],
+    description: "JS library for interacting with the dusk network",
+    license: "MPL",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/dusk-network/dusk-wallet-js",
+    },
+    bugs: {
+      url: "https://github.com/dusk-network/dusk-wallet-js/issues",
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
