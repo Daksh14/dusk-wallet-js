7ffd6cb122b610568e78096fe41b8d4c98d0b16c
Daksh <work@dakshu.xyz>
2023-12-10 02:29:35 -0500

    Add jsdoc.json

---
+++ jsdoc.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  source: {
+    include: [
+      "src/history.js",
+      "src/balance.js",
+      "src/db.js",
+      "src/mnemonic.js",
+      "src/mod.js",
+    ],
+  },
+};
