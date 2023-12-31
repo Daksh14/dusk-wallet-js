a6bb6db07bd740ca301e32cad0b612f5c9f4b9f1
Daksh <work@dakshu.xyz>
2023-12-30 19:23:58 -0500

    Fix tests

--- tests/balance_test.js
+++ tests/balance_test.js
@@ -232,7 +232,7 @@ Deno.test({
       assertEquals(history[1].direction, "Out");
       assertEquals(parseFloat(history[1].fee, 10), history[1].fee);
       assertEquals(history[1].id.length, 64);
-      assertEquals(new String(history[1].tx_type), history[1].tx_type);
+      assert(history[1].tx_type == "WITHDRAW" || history[1].tx_type == "ALLOW");
 
       assertEquals(parseFloat(history[2].amount), history[2].amount);
       assertEquals(
@@ -242,7 +242,9 @@ Deno.test({
       assertEquals(history[2].direction, "Out");
       assertEquals(parseFloat(history[2].fee, 10), history[2].fee);
       assertEquals(history[2].id.length, 64);
-      assertEquals(new String(history[2].tx_type), history[2].tx_type);
+      assertEquals(
+        history[2].tx_type == "WITHDRAW" || history[2].tx_type == "ALLOW"
+      );
     });
   },
   sanitizeResources: false,
